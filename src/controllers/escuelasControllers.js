25a80687c11d536a46d8e2ec0aabd8cbee022d5c
caroko9 <caroko9@hotmail.com>
2023-07-14 16:44:37 -0300

    libreria uuid instalada para generar un id por escuela registrada

--- src/controllers/escuelasControllers.js
+++ src/controllers/escuelasControllers.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const escuelasFilePath = path.join(__dirname, '../../src/data/escuelas.json');
-
+//JSON CON LA LISTA DE ESCUELAS
 let escuelas = [];
 
 if (fs.existsSync(escuelasFilePath)) {
@@ -22,12 +22,13 @@ const controller = {
   sumaEscuela: (req, res) => {
     res.render("escuelascreate");
   },
-
+//FORMULARIO ESCUELA NUEVA
 	creaEscuela: (req, res) => {
 
 		let escuelaNueva = req.body;
 	
 		let objNuevaEscuela= {
+      id: generarId(),
       nombre: escuelaNueva.nombre,
 			email: escuelaNueva.email,
 			descripcion: escuelaNueva.descripcion,
@@ -38,19 +39,23 @@ const controller = {
 	    escuelas.push(objNuevaEscuela);
 		fs.writeFileSync(escuelasFilePath, JSON.stringify(escuelas,null,' '));
     res.redirect("./escuelasList"); 
-	},
 
+    function generarId() {
+
+      const { v4: uuidv4 } = require('uuid');
+      return uuidv4();
+    }
+
+	},
+//VISTA DE LA LISTA DE ESCUELAS
   list: (req, res) => {
     res.render("escuelasList", { escuelasRegistradas: escuelas });
   },
 
-
+//MÉTODO USADO PARA MOSTRAR LAS ESCUELAS FILTRADAS EN LA BARRA DE BÚSQUEDA DE LA VISTA ANTERIOR
 buscarEscuela: (req, res) => {
   let escuelaEncontrada = req.query.buscar;
   let escuelasBuscadasArray = [];
-
-console.log("Hola" , escuelas);
-console.log("Hola dos" , escuelaEncontrada);
 
   for (let i = 0; i < escuelas.length; i++) {
     if ( (escuelas[i].nombre.includes(escuelaEncontrada.toUpperCase())) 
--- src/routes/escuelasRoutes.js
+++ src/routes/escuelasRoutes.js
@@ -8,11 +8,12 @@ router.get('/escuelascreate', escuelasController.sumaEscuela);
 
 router.post('/escuelascreate', escuelasController.creaEscuela);
 
-router.get('/escuelasList', escuelasController.list);
+router.get('/escuelasList', escuelasController.list); //muestra el listado de escuela
 
-router.get('/escuelasResults', escuelasController.buscarEscuela);
+router.get('/escuelasResults', escuelasController.buscarEscuela); //muestra el resultado de las escuelas buscadas por el usuario
 
-//router.post('/escuelas', escuelasController.creaEscuela)
+//router.get('/escuelasList/id', escuelasController.detalleEscuela); // muestra el detalle por escuela
+
 
 
 
