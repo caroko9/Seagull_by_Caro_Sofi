fb5af4d920d5420c09e1719e9ba745a89032c7cf
caroko9 <caroko9@hotmail.com>
2023-09-15 19:31:01 -0300

    .env agregado

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,25 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "seagullcs",
-    "password": "seagull.23",
-    "database": "seagullcs_db",
-    "host": "mysql-seagullcs.alwaysdata.net",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.CB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "mysql-seagullcs.alwaysdata.net",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.CB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "mysql-seagullcs.alwaysdata.net",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.CB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
   }
 }
