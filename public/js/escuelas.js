029f29ac4cc3724b855d238f4ca6bedfc4711559
caroko9 <caroko9@hotmail.com>
2023-10-18 21:45:31 -0300

    validaciones js form escuelas funcionando

--- public/js/escuelas.js
+++ public/js/escuelas.js
@@ -1,22 +1,47 @@
 
 window.addEventListener('load', function () {
 
-    //intentar borrando el require del formulario
     let formulario = document.querySelector('#formescuela');
 
-    formulario.addEventListener("submit", function(e) {
+    formulario.addEventListener("submit", function (e) {
         e.preventDefault();
 
         let nombreInput = document.getElementById('nombre');
-
+        let emailInput = document.getElementById("email");
+        let telefonoInput = document.getElementById("telefono");
+        let paginaWebInput = document.getElementById("paginaWeb");
+        let descripcionInput = document.getElementById("descripcion");
+        let ubicacionInput = document.getElementById("ubicacion");
+        let paisInput = document.getElementById("pais");
+        
+               
 
         if (nombreInput.value == "") {
-            alert("Este campo no puede estar vacío");
-            return;
+            alert("Por favor revisa y completa todos los campos");
         }
-          formulario.submit()
+        else if (emailInput.value == "") {
+            alert("Por favor revisa y completa todos los campos");
+        }
+        else if (telefonoInput.value == "") {
+            alert("Por favor revisa y completa todos los campos");
+        }
+        else if (paginaWebInput.value == "") {
+            alert("Por favor revisa y completa todos los campos");
+        }
+        else if (descripcionInput.value == "") {
+            alert("Por favor revisa y completa todos los campos");
+        }
+         else if (ubicacionInput.value == "") {
+            alert("Por favor revisa y completa todos los campos");
+        } 
+        else if (paisInput.value == "") {
+            alert("Por favor revisa y completa todos los campos");
+        }
 
-    })
+    formulario.submit()
 
-})
+    }
+    )}
+)
 
+  
